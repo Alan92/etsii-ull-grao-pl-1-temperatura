7c7a47faf831df847b8e23db740a019602476388
Alan92 <wensescalo4@gmail.com>
2013-02-10 20:26:42 +0000

    Update temperature.js

--- temperature.js
+++ temperature.js
@@ -1,23 +1,21 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
 function calculate() {
   var result;
-  var original       = document.getElementById("........");
+  var original = document.getElementById("original");
   var temp = original.value;
-  var regexp = /.............................../;
-  
-  var m = temp.match(......);
-  
+  var regexp = /(-[0-9]+|[0-9]+)(\s*|\.)([0-9]*)(C|F|c|f)/;
+  var m = temp.match(regexp);
   if (m) {
-    var num = ....;
-    var type = ....;
+    var num = m[1]+m[2]+m[3];
+    var type = m[4];
     num = parseFloat(num);
     if (type == 'c' || type == 'C') {
       result = (num * 9/5)+32;
-      result = ..............................
+      result = result + 'F';
     }
     else {
       result = (num - 32)*5/9;
-      result = ............................
+      result = result + 'C';
     }
     converted.innerHTML = result;
   }
@@ -25,3 +23,8 @@ function calculate() {
     converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
   }
 }
+
+
+
+
+
