ac4f41c2f67a756f4f183835490c911758478dd2
Alan92 <wensescalo4@gmail.com>
2013-02-10 20:49:19 +0000

    Update temperature.js

--- temperature.js
+++ temperature.js
@@ -3,7 +3,7 @@ function calculate() {
   var result;
   var original = document.getElementById("original");
   var temp = original.value;
-  var regexp = /(-[0-9]+|[0-9]+)(\s*|\.)([0-9]*)(C|F|c|f)/;
+  var regexp = /^(-[0-9]+|[0-9]+)(\s*|\.)([0-9]*)(C|F|c|f)$/;
   var m = temp.match(regexp);
   if (m) {
     var num = m[1]+m[2]+m[3];
